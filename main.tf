a8820db6e763f23bbd3edc0df8bd1eeea5ca0dcf
Mostafa <m.torbon@gmx.de>
2020-11-14 16:45:41 +0100

    First terraform version - loadbalancer not working atm

---
+++ main.tf
@@ -0,0 +1,73 @@
+# Configure the Docker provider
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_network" "nginxweb" {
+  name = "nginxweb"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx"
+  keep_locally = true
+}
+
+resource "docker_container" "hello1" {
+  image = docker_image.nginx.latest
+  name = "hello1"
+  volumes {
+    host_path = "${abspath(path.root)}/hello1"
+    container_path = "/usr/share/nginx/html"
+    read_only = true
+  }
+  network_mode = "nginxweb"
+  ports {
+    internal = "80"
+    external = "8081"
+    protocol = "tcp"
+    ip = "0.0.0.0"
+  }
+} 
+
+resource "docker_container" "hello2" {
+  image = docker_image.nginx.latest
+  name = "hello2"
+  volumes {
+    host_path = "${abspath(path.root)}/hello2"
+    container_path = "/usr/share/nginx/html"
+    read_only = true
+  }
+  network_mode = "nginxweb"
+  ports {
+    internal = "80"
+    external = "8082"
+    protocol = "tcp"
+    ip = "0.0.0.0"
+  }
+} 
+
+resource "docker_container" "nginxlb" {
+  image = docker_image.nginx.latest
+  name = "nginxlb"
+  volumes {
+    host_path = "${abspath(path.root)}/nginx/conf"
+    container_path = "/etc/nginx/conf.d"
+  }
+  volumes {
+    host_path = "${abspath(path.root)}/nginx/keys"
+    container_path = "/etc/nginx"
+  }
+  network_mode = "nginxweb"
+  ports {
+    internal = "80"
+    external = "80"
+    protocol = "tcp"
+    ip = "0.0.0.0"
+  }
+  ports {
+    internal = "443"
+    external = "443"
+    protocol = "tcp"
+    ip = "0.0.0.0"
+  }
+}
